d70ef80f6e5b629f9c7272343cf6a98c6b068599
Jakub Lotko <1elstac1@gmail.com>
2019-10-23 00:54:32 +0200

    Add ProductDto

---
+++ OnionArchitectureSample.Application/Contracts/Dtos/ProductDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OnionArchitectureSample.Application.Contracts.Dtos
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
