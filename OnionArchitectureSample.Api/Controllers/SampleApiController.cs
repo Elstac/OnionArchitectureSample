8bc628dba4d0b78872d6c5ded2d7d682d5d7fe92
Jakub Lotko <1elstac1@gmail.com>
2019-10-22 23:01:52 +0200

    Replace WheatercastController with Sample one

---
+++ OnionArchitectureSample.Api/Controllers/SampleApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace OnionArchitectureSample.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleApiController : ControllerBase
+    {
+        [HttpGet("getAll")]
+        public IEnumerable<string> GetAll()
+        {
+            return new string[] { "A", "B" };
+        }
+    }
+}
--- OnionArchitectureSample.Api/Startup.cs
+++ OnionArchitectureSample.Api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace OnionArchitectureSample.Api
 {
@@ -26,6 +20,12 @@ namespace OnionArchitectureSample.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +36,15 @@ namespace OnionArchitectureSample.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            //Enable swagger
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            //Other stuff
             app.UseHttpsRedirection();
 
             app.UseRouting();
